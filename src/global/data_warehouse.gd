b1776724f1c27bf620e26614d2f3fa9280a0bcbb
Rokle <roklefit@gmail.com>
2025-07-19 23:17:19 +0300

    Added player module + data_warehouse

---
+++ src/global/data_warehouse.gd
@@ -0,0 +1,35 @@
+extends Node
+# Storage for all data_holders
+# Doesn't do anything else
+
+#region ------------------------ PRIVATE VARS ----------------------------------
+
+var _player_data_holder: PlayerDataHolder
+
+#endregion
+
+#region ======================== SET UP METHODS ================================
+
+
+func _ready() -> void:
+    _set_up()
+
+
+func _set_up() -> void:
+    _set_ready_variables()
+
+
+func _set_ready_variables() -> void:
+    _player_data_holder = PlayerDataHolder.new()
+
+
+#endregion
+
+#region ======================== PUBLIC METHODS ================================
+
+
+func get_plaeyr_data_holder() -> PlayerDataHolder:
+    assert(_player_data_holder != null, "Player data holder missing")
+    return _player_data_holder
+
+#endregion
---
+++ src/modules/player_module/player_data_holder.gd
@@ -0,0 +1,69 @@
+class_name PlayerDataHolder
+extends Node
+
+# Interfaces:
+#
+# func add_xp(number: float) -> void:
+# DataWarehouse.get_player_data_holder().add_xp()
+
+#region ------------------------ PRIVATE VARS ----------------------------------
+
+var _data: PlayerData
+
+#endregion
+
+#region ======================== SET UP METHODS ================================
+
+
+func _init() -> void:
+    _set_up()
+
+
+func _set_up() -> void:
+    _data = PlayerData.new()
+    # TODO think about what data should be saved
+
+
+#endregion
+
+#region ======================== PUBLIC METHODS ================================
+
+# Place for all functions that modify data
+# Try to create function for every action and don’t modify data directly
+
+
+func add_xp(number: float) -> void:
+    _data.current_level_xp += number
+    _try_level_up()
+
+
+#endregion
+
+#region ======================== PRIVATE METHODS ===============================
+
+
+func _try_level_up() -> void:
+    const MAX_LEVEL_UP_COUNT = 10000
+    var current_count = 0
+    while _can_level_up():
+        _level_up()
+        current_count += 1
+        if current_count >= MAX_LEVEL_UP_COUNT:
+            assert(false, "Exp is probably glitched")
+            break
+
+
+func _level_up() -> void:
+    _data.level += 1
+    _data.current_level_xp -= _get_xp_to_level_up(_data.level)
+    EventBus.played_leved_up.emit()
+
+
+func _can_level_up() -> bool:
+    return _data.current_level_xp <= _get_xp_to_level_up(_data.level)
+
+
+func _get_xp_to_level_up(level: int) -> float:
+    return level * 10
+
+#endregion
