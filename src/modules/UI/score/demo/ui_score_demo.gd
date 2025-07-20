361f2902d146e7867f135fabefeb7af6038dc484
CodeLitschi <CodeLitschi@gmail.com>
2025-07-20 22:27:28 +0200

    fix: Scaling

--- src/modules/UI/score/demo/ui_score_demo.gd
+++ src/modules/UI/score/demo/ui_score_demo.gd
@@ -1,21 +1,21 @@
-extends Node2D
+extends Node
 var r_score: int = 0
 var l_score: int = 0
 var show_score: bool = true
 
 
 func _input(event):
-    if event is InputEventMouseButton and event.pressed:
-        if event.button_index == MOUSE_BUTTON_MIDDLE:
-            if show_score:
-                EventBus.emit_signal("ui_show_score")
-            else:
-                EventBus.emit_signal("ui_hide_score")
-            show_score = !show_score
-            return
-        if event.button_index == MOUSE_BUTTON_LEFT:
-            l_score += 1
-        if event.button_index == MOUSE_BUTTON_RIGHT:
-            r_score += 1
-        var mouse_pos = get_viewport().get_mouse_position()
-        EventBus.emit_signal("ui_set_score", l_score, r_score, mouse_pos)
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == MOUSE_BUTTON_MIDDLE:
+			if show_score:
+				EventBus.emit_signal("ui_show_score")
+			else:
+				EventBus.emit_signal("ui_hide_score")
+			show_score = !show_score
+			return
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			l_score += 1
+		if event.button_index == MOUSE_BUTTON_RIGHT:
+			r_score += 1
+		var mouse_pos = get_viewport().get_mouse_position()
+		EventBus.emit_signal("ui_set_score", l_score, r_score, mouse_pos)
