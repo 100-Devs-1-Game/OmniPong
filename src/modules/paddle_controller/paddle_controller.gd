e590c4a2ccdc2a85868902a7994c0cba2c3347df
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-07-20 03:29:08 +0200

    Fix formatting

--- src/modules/paddle_controller/paddle_controller.gd
+++ src/modules/paddle_controller/paddle_controller.gd
@@ -3,12 +3,12 @@ extends Node
 
 
 func get_vertical_input() -> float:
-	return 0.0
+    return 0.0
 
 
 func get_tilt_input() -> float:
-	return 0.0
+    return 0.0
 
 
 func get_look_vector(_from_position: Vector2) -> Vector2:
-	return Vector2.ZERO
+    return Vector2.ZERO
