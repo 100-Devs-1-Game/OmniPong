3d82fd467313f3ec3ed169117e15b70507e8978d
Nicolai Moore <niconorsk@gmail.com>
2025-07-20 18:13:22 +1000

    Tied enough things togethers that we have something that is definitely pong and runs

--- src/modules/game_manager/game_manager.gd
+++ src/modules/game_manager/game_manager.gd
@@ -16,6 +16,8 @@ func _ready() -> void:
 func handle_opponent_spawn():
     var opponent_paddle: Node = paddle_scene.instantiate()
     self.add_child(opponent_paddle)
+    opponent_paddle.paddle_controller_script = load("res://modules/AI/ai_paddle_controller.gd")
+    opponent_paddle.is_player = false
     var right_middle_of_screen: Vector2 = Vector2(viewport_size.x, viewport_size.y / 2)
     opponent_paddle.position = right_middle_of_screen
     opponent_paddle.rotation_degrees = 180
@@ -25,6 +27,10 @@ func handle_opponent_spawn():
 func handle_player_spawn():
     var player_paddle: Node = paddle_scene.instantiate()
     self.add_child(player_paddle)
+    player_paddle.paddle_controller_script = load(
+        "res://modules/paddle_controller/player_paddle_controller.gd"
+    )
+    player_paddle.is_player = true
     var left_middle_of_screen: Vector2 = Vector2(0, viewport_size.y / 2)
     player_paddle.position = left_middle_of_screen
     EventBus.set_paddle_facing_right.emit(true, true)
@@ -33,4 +39,5 @@ func handle_player_spawn():
 func handle_ball_spawn():
     var ball: Node = ball_scene.instantiate()
     self.add_child(ball)
-    ball.position = get_viewport_rect().size / 2
+    EventBus.set_ball_position.emit(get_viewport_rect().size / 2)
+    EventBus.set_ball_velocity.emit(Vector2(-1, 0) * 200)
