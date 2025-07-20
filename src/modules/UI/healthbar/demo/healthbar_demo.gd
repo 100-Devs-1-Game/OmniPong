af1f5e2dded8825eb44777f932689950c5dc8792
CodeLitschi <CodeLitschi@gmail.com>
2025-07-20 23:36:35 +0200

    Add healthbar without animations

---
+++ src/modules/UI/healthbar/demo/healthbar_demo.gd
@@ -0,0 +1,20 @@
+extends Node
+
+var r_health: int = 100
+var l_health: int = 100
+var show_health: bool = true
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == MOUSE_BUTTON_MIDDLE:
+			if show_health:
+				EventBus.emit_signal("ui_show_healthbar")
+			else:
+				EventBus.emit_signal("ui_hide_healthbar")
+			show_health = !show_health
+			return
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			l_health -= 10
+		if event.button_index == MOUSE_BUTTON_RIGHT:
+			r_health -= 10
+		EventBus.emit_signal("ui_set_healthbar", l_health, r_health)
---
+++ src/modules/UI/healthbar/healthbar.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	EventBus.connect("ui_set_healthbar", Callable(self, "ui_set_healthbar"))
+	EventBus.connect("ui_hide_healthbar", Callable(self, "ui_hide_healthbar"))
+	EventBus.connect("ui_show_healthbar", Callable(self, "ui_show_healthbar"))
+
+
+func ui_hide_healthbar() -> void:
+	self.hide()
+
+
+func ui_show_healthbar() -> void:
+	self.show()
+
+func ui_set_healthbar(left: int, right: int) -> void:
+	_update_healthbar_value(left,right)
+
+func _update_healthbar_value(left: int, right: int) -> void:
+	($Left as TextureProgressBar).value = left
+	($Right as TextureProgressBar).value = right
