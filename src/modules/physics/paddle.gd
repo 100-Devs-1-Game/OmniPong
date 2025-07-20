6003d73cd2240feda9947258f24f67049eb27260
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-07-20 03:17:02 +0200

    Fix formatting

--- src/modules/physics/paddle.gd
+++ src/modules/physics/paddle.gd
@@ -11,30 +11,30 @@ var controller: PaddleController
 var velocity: float
 
 var is_player := false:
-	set(b):
-		assert(is_inside_tree())
-		is_player = b
-		if is_player:
-			initialize_player_controller(InputPaddleController.ControlScheme.KEYBOARD_AND_MOUSE)
+    set(b):
+        assert(is_inside_tree())
+        is_player = b
+        if is_player:
+            initialize_player_controller(InputPaddleController.ControlScheme.KEYBOARD_AND_MOUSE)
 
 
 func _physics_process(delta: float) -> void:
-	velocity = move_toward(
-		velocity, controller.get_vertical_input() * get_current_speed(), acceleration * delta
-	)
-	position.y += velocity * delta
-	var look_vec := controller.get_look_vector(position)
+    velocity = move_toward(
+        velocity, controller.get_vertical_input() * get_current_speed(), acceleration * delta
+    )
+    position.y += velocity * delta
+    var look_vec := controller.get_look_vector(position)
 
-	look_at(position + look_vec)
+    look_at(position + look_vec)
 
 
 func get_current_speed() -> float:
-	return max_move_speed
+    return max_move_speed
 
 
 func initialize_player_controller(scheme: InputPaddleController.ControlScheme):
-	var node = Node.new()
-	node.set_script(paddle_controller_script)
-	add_child(node)
-	controller = node as InputPaddleController
-	controller.control_scheme = InputPaddleController.ControlScheme.KEYBOARD_AND_MOUSE
+    var node = Node.new()
+    node.set_script(paddle_controller_script)
+    add_child(node)
+    controller = node as InputPaddleController
+    controller.control_scheme = InputPaddleController.ControlScheme.KEYBOARD_AND_MOUSE
