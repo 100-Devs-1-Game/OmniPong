7c368e326d753208d19d4aa6f1d619c86327c857
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-07-20 03:57:20 +0200

    Update test

--- src/modules/physics/tests/move_paddle/paddle_and_ball_test.gd
+++ src/modules/physics/tests/move_paddle/paddle_and_ball_test.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
 @onready var paddle: Paddle = $Paddle
+@onready var ball: Ball = $Ball
 
 
 func _ready() -> void:
     paddle.is_player = true
+    ball.velocity= Vector2(-100, 0)
