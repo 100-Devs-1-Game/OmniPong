e0d0731ed6a29fcea148fc5c157f9ccc09929c1e
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-07-20 03:56:55 +0200

    Implement collision handling

--- src/modules/physics/ball.gd
+++ src/modules/physics/ball.gd
@@ -1,11 +1,22 @@
 class_name Ball
-extends Node2D
+extends CharacterBody2D
 
-var velocity: Vector2
+@export var wall_margin: float = 50.0
 
 
 func _physics_process(delta: float) -> void:
-    position += velocity * delta
+    var motion: Vector2 = velocity * delta
+    var new_position = position + motion
+
+    if motion.y < 0 and new_position.y < wall_margin:
+        velocity = velocity.bounce(Vector2.DOWN)
+    if motion.y > 0 and new_position.y > get_viewport().get_visible_rect().size.y + wall_margin:
+        velocity = velocity.bounce(Vector2.UP)
+
+    var collision := move_and_collide(motion)
+
+    if collision:
+        velocity = velocity.bounce(collision.get_normal())
 
     EventBus.updated_ball_velocity.emit(velocity)
     EventBus.updated_ball_position.emit(position)
